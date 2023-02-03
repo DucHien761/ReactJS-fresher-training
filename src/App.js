2e519df43fcaf1d53ed396f7178de4bae817b32f
Hien Vo <hien.vo@s3corp.com.vn>
2023-02-03 11:08:45 +0700

    A1-research var let const

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var times = 5;
+  var test1 = "hey test1";
+  let test2 = "hey test2";
+  const test3 = "hey test3";
+  // Đối với trường hợp kiểu biến là reference (bao gồm object, array, và function) thì tuy không thể tái khai báo hay cập nhật giá trị của biến nhưng chúng ta vẫn có thể cập nhật giá trị cho thuộc tính của biến đó.
+  const greeting = {
+    message: "Hello",
+    number: "five",
+  };
+
+  greeting.message = "say Hello instead";
+  console.log(greeting);
+  // {message:"say Hello instead",number:"five"}
+
+  const test = () => {
+    let test2 = "hey hien";
+    if (times > 3) {
+      test2 = "say Hi";
+      test1 = "hello";
+      let test4 = "hey test4";
+    }
+    console.log(test1);
+    console.log(test2);
+    console.log(test3);
+    //console.log(test4); //  'test4' is not defined because test4 in a blocked scope
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={test}>Submit</button>
     </div>
   );
 }
